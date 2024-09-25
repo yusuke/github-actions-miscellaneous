00b19e95b3d877e9c52c3849fec9a395548211fb
Yusuke Yamamoto <yusuke@mac.com>
2024-09-25 17:38:48 +0900

    テスト失敗させる

--- call_test.go
+++ call_test.go
@@ -9,14 +9,14 @@ import (
 func TestParseKeyValue(t *testing.T) {
 	kv, err := ParseKeyValue("foo = bar")
 	assert.Nil(t, err)
-	assert.Equal(t, Key("foo"), kv.Key())
+	assert.NotEqual(t, Key("foo"), kv.Key())
 	assert.Equal(t, Value("bar"), kv.Value())
 }
 
 func TestParseKeyValue2(t *testing.T) {
 	kv, err := ParseKeyValue("GITHUB = fun")
 	assert.Nil(t, err)
-	assert.Equal(t, Key("GITHUB"), kv.Key())
+	assert.NotEqual(t, Key("GITHUB"), kv.Key())
 	assert.Equal(t, Value("fun"), kv.Value())
 }
 
